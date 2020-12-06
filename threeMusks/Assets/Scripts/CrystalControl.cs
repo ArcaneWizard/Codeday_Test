d853943a232e8304246c665f218f62f63d080c68
Happyday12 <Happyday12@users.noreply.github.com>
2020-12-06 06:30:36 -0800

    crystals cracked

---
+++ threeMusks/Assets/Scripts/CrystalControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalControl : MonoBehaviour
+{
+
+    public int health = 20;
+    public Material shiny;
+    public Material cracked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 10)
+        {
+            transform.gameObject.GetComponent<MeshRenderer>().material = cracked;
+        }
+    }
+}
